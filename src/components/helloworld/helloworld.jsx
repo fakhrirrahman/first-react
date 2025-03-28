969c7e57036a28c1e1b875551cc5a007e170ed11
Fakhrir Rahman <fakhrirrahman7@gmail.com>
2025-03-28 18:44:35 +0700

    baru sampai menit ke 48:57 PZM

--- src/components/helloworld/helloworld.jsx
+++ src/components/helloworld/helloworld.jsx
@@ -1,8 +1,46 @@
 export default function HelloWorld() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <p>saya fakhrir belajar react js pakai jsx</p>
+      <HeaderHelloWorld text="ini object hello world" />
+      <ContentHelloWorld />
+      <FooterHelloWorld />
+    </div>
+  );
+}
+
+function HeaderHelloWorld({ text }) {
+  const headerStyle = {
+    color: "red",
+    backgroundColor: "blue",
+    padding: "10px",
+    borderRadius: "5px",
+    textAlign: "center",
+    margin: "0",
+    fontFamily: "Arial, sans-serif",
+    fontSize: "24px",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+  };
+
+  return (
+    <div>
+      <h1 style={headerStyle}>{text.toLowerCase()}</h1>
+    </div>
+  );
+}
+
+function ContentHelloWorld() {
+  return (
+    <div>
+      <p>Ini adalah content dari Hello World</p>
+    </div>
+  );
+}
+
+function FooterHelloWorld() {
+  return (
+    <div>
+      <p>Ini adalah footer dari Hello World</p>
     </div>
   );
 }
--- src/components/helloworld/main.jsx
+++ src/components/helloworld/main.jsx
@@ -1,6 +1,11 @@
 import { createRoot } from "react-dom/client";
 import HelloWorld from "./helloworld.jsx";
+import { StrictMode } from "react";
 
 // Create a root DOM element and render the HelloWorld component into it
 
-createRoot(document.getElementById("root")).render(<HelloWorld />);
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <HelloWorld />
+    </StrictMode>
+);
