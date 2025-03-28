823440c0c235bf02913e9e24907a64449ca10871
Fakhrir Rahman <fakhrirrahman7@gmail.com>
2025-03-28 18:24:37 +0700

    first commit

---
+++ src/components/helloworld/helloworld.jsx
@@ -0,0 +1,8 @@
+export default function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>saya fakhrir belajar react js pakai jsx</p>
+    </div>
+  );
+}
---
+++ src/components/helloworld/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import HelloWorld from "./helloworld.jsx";
+
+// Create a root DOM element and render the HelloWorld component into it
+
+createRoot(document.getElementById("root")).render(<HelloWorld />);
